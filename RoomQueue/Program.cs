c17f29b61e53cf8cf72e62c2968c39097662b861
soulflysk <soulflysk@outlook.co.th>
2022-11-01 13:36:11 +0700

    fix enqueue

--- RoomQueue/Program.cs
+++ RoomQueue/Program.cs
@@ -9,21 +9,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        //timestamp ขาเข้าไม่ต้องมี แต่ต้องมี timestamp ขาออก (timestamp เป็น timestamp ขาออก)
         // ผู้ถูกนวดเข้ามาในห้อง
         Queue<Person> peopleQ = new Queue<Person>();
-        peopleQ.Enqueue(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47, Timestamp = "2022‑09‑25 17:45:30" });
-        peopleQ.Enqueue(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45, Timestamp = "2022‑09‑25 18:20:18" });
-        peopleQ.Enqueue(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9, Timestamp = "2022‑09‑25 19:17:50" });
-        // Peek at first person in Q.
-        Console.WriteLine("{0} is first in line!", peopleQ.Peek().FirstName);
-        // ผู้ถูกนวดออกจากห้อง
-        GetRoom(peopleQ.Dequeue());
-        GetRoom(peopleQ.Dequeue());
-        GetRoom(peopleQ.Dequeue());
-        
-        //ผู้ถูกนวดใหม่เข้ามาในห้องที่ผู้ถูกนวดเก่าออกไปนานที่สุด 
-        peopleQ.Enqueue(new Person { FirstName = "Tanade", LastName = "Korwanitgun", Age = 33, Timestamp = "2022‑11‑01 12:25:56" });
 
+
+
+        // ผู้ถูกนวดออกจากห้อง ผู้ถูกนวดใหม่เข้ามาในห้องที่ผู้ถูกนวดเก่าออกไปนานที่สุด 
+        peopleQ.Enqueue(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47, Timestamp = "2022‑09‑25 17:45:30" });
+        GetRoom(peopleQ.Dequeue());
+        peopleQ.Enqueue(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45, Timestamp = "2022‑09‑25 18:20:18" });
+        GetRoom(peopleQ.Dequeue());
+        peopleQ.Enqueue(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9, Timestamp = "2022‑09‑25 19:17:50" });
+        GetRoom(peopleQ.Dequeue());
         
         try
         {
@@ -37,8 +35,9 @@ class Program
     }
     static void GetRoom(Person p)
     {
-        Console.WriteLine("{0} got room!", p.FirstName);
-        //sort timestamp เก่าสุด
+        //sort timestamp, เก่าสุด ก่อน
         //???
+        Console.WriteLine("{0} got room!", p.FirstName);
+        
     }
 }
